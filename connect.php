e8c114024bdb97c596f020ad5563b3bfd26745bd
azurecie <adityajanata@outlook.com>
2017-02-23 13:43:16 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$name = "dodol_db";
+$host = "ap-cdbr-azure-southeast-b.cloudapp.net";
+$user = "b908fe73ad502e";
+$pass = "ff6c8be8";
+$name = "fajarwebappdb2";
  
 $koneksi = mysql_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
 mysql_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
